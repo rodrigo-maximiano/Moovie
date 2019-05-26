92a16323bb5585233475691992e6b4c414c8fd86
JohnnyLutz <contato@joaolutz.com>
2019-05-26 17:05:44 -0300

    Adicionada classe de modelo para responde da API.

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/moovie/model/Response.kt
@@ -0,0 +1,34 @@
+package br.edu.ifsp.scl.sdm.moovie.model
+
+data class OmdbResponse (
+    val title : String,
+    val year : Int,
+    val rated : String,
+    val released : String,
+    val runtime : String,
+    val genre : String,
+    val director : String,
+    val writer : String,
+    val actors : String,
+    val plot : String,
+    val language : String,
+    val country : String,
+    val awards : String,
+    val poster : String,
+    val ratings : List<Ratings>,
+    val metascore : Int,
+    val imdbRating : Double,
+    val imdbVotes : String,
+    val imdbID : String,
+    val type : String,
+    val dVD : String,
+    val boxOffice : String,
+    val production : String,
+    val website : String,
+    val response : Boolean
+)
+
+data class Ratings (
+    val source : String,
+    val value : String
+)
