867e29b09dc4bf337a0eedb6cf07b1e0ccd48caf
JohnnyLutz <contato@joaolutz.com>
2019-06-03 20:53:51 -0300

    Subindo arquivos do projeto.

--- app/src/main/java/br/edu/ifsp/scl/sdm/moovie/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/moovie/MainActivity.kt
@@ -1,12 +1,74 @@
-package br.edu.ifsp.scl.sdm.moovie
+package br.edu.ifsp.scl.sdm.dicesdm
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.Menu
+import android.view.MenuItem
+import br.edu.ifsp.scl.sdm.moovie.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
+import org.jetbrains.anko.startActivity
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+
+    // Gerador de números randômicos usado para simular o lançamento do dado
+    val geradorRandomico: Random = Random(System.currentTimeMillis())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.frame_main)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = resources.getString(R.string.app_name)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val abreFechaToggle: ActionBarDrawerToggle =
+                ActionBarDrawerToggle(this,
+                        menuLateralDrawerLayout,
+                        toolbar,
+                        R.string.menu_aberto,
+                        R.string.menu_fechado
+                )
+        menuLateralDrawerLayout.addDrawerListener(abreFechaToggle)
+        abreFechaToggle.syncState()
+
+        menuNavigationView.setNavigationItemSelectedListener { onNavigationItemSelected(it) }
     }
+
+    fun onNavigationItemSelected(it: MenuItem): Boolean {
+        var retorno: Boolean = false
+        when(it.itemId) {
+            R.id.modoTextoMenuItem -> {
+                //TODO()
+                retorno = true
+            }
+            R.id.modoGraficoMenuItem -> {
+                TODO()
+                retorno = true
+            }
+            R.id.sairMenuItem -> {
+                finish()
+                retorno = true
+            }
+        }
+        menuLateralDrawerLayout.closeDrawer(GravityCompat.START)
+        return retorno
+    }
+
+    override fun onBackPressed() {
+        if(menuLateralDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            menuLateralDrawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
