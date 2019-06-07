2b8db0ebdb55111526747f9d74e490a21980c336
Joao Messi <joao.messi@movemais.com>
2019-06-07 10:14:43 -0300

    Ajustes finais de layout e remoção de constantes não utilizadas.

--- app/src/main/java/br/edu/ifsp/scl/sdm/moovie/Constantes.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/moovie/Constantes.kt
@@ -3,6 +3,5 @@ package br.edu.ifsp.scl.sdm.moovie
 object Constantes {
     // Constantes usadas para acesso ao WS de tradução
     val URL_BASE = "http://www.omdbapi.com"
-    val APP_KEY_FIELD = "apikey"
-    val APP_KEY_VALUE = "90aa25a0" // Preeencher com seu app_key
+    val APP_KEY_VALUE = "90aa25a0" // Preencher com seu app_key
 }
